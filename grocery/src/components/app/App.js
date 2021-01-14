08b1330ca05acac741e45b24253f13d1e9f78acc
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2021-01-14 13:48:08 -0700

    small tweaks

--- grocery/src/components/app/App.js
+++ grocery/src/components/app/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import {Switch } from 'react-router-dom'
 
 import LandingRoute from '../routes/landingRoute';
 import ProductRoute from '../routes/productRoute'
--- grocery/src/components/routes/productRoute.js
+++ grocery/src/components/routes/productRoute.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Product from '../product/product'
 
 class ProductRoute extends Product {
